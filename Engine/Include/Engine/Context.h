1597c8b2b7c8d14081dcc32e778cb1a7b5b190a3
larso0 <lars.olav.tolo@gmail.com>
2017-03-02 13:40:52 +0100

    Context and window interface.

---
+++ Engine/Include/Engine/Context.h
@@ -0,0 +1,15 @@
+#ifndef ENGINE_CONTEXT_H
+#define ENGINE_CONTEXT_H
+
+#include <Engine/Window.h>
+
+namespace Engine {
+	class Context {
+	public:
+		virtual ~Context() {}
+
+		virtual Window* createWindow(int w, int h, int flags) = 0;
+	};
+}
+
+#endif
---
+++ Engine/Include/Engine/Window.h
@@ -0,0 +1,19 @@
+#ifndef ENGINE_WINDOW_H
+#define ENGINE_WINDOW_H
+
+#include <Engine/Renderer.h>
+#include <cstdint>
+
+namespace Engine {
+	class Window {
+	public:
+		virtual ~Window() {}
+
+		virtual void resize(uint32_t w, uint32_t h) = 0;
+		virtual uint32_t getWidth() const = 0;
+		virtual uint32_t getHeight() const = 0;
+		virtual Renderer& getRenderer() = 0;
+	};
+}
+
+#endif
